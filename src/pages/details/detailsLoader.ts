ed3357e4520ab1fa5ec184054d0ea12f89b76694
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-07-25 19:35:35 -0500

    completed work on details page

--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,12 +1,17 @@
 import {getPackage} from "../../api/queries/getPackage";
 import type {Params} from "react-router-dom";
+import type {PackageDetails} from "../../api/types/packageDetails";
 
 interface LoaderArgs {
     params:Params
 }
 
+export interface DetailsLoaderResult {
+    details:PackageDetails
+}
+
 //params pulls from the wildcard after /packages/ url
-export async function detailsLoader({params}:LoaderArgs) {
+export async function detailsLoader({params}:LoaderArgs):Promise<DetailsLoaderResult> {
     const {name} = params;
 
     if (!name){
