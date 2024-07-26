e51899ba5df0cc711b6c75514960b018907129d3
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-07-25 20:43:43 -0500

    completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Root from "./pages/Root";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
+import {homeLoader} from "./pages/home/homeLoader";
 import SearchPage from "./pages/search/SearchPage";
 import {searchLoader} from "./pages/search/searchLoader";
 import DetailsPage from "./pages/details/DetailsPage";
@@ -14,7 +15,8 @@ const router = createBrowserRouter([
   children: [
    {
     index:true, //any route that is not /search or /packages/xyz shows HomePage
-    element:<HomePage/>
+    element:<HomePage/>,
+    loader: homeLoader
    },
    {
     path:'/search', //if user goes to /search, show SearchPage
